26ce357c640329237577fcbe7410fa2694dfbc0f
Hazel <basile.cunuder@gmail.com>
2025-05-06 16:01:03 +0200

    feat(jour-05-exos.js): add exercise js.info 2.5

--- Semaine-02/jour-05/jour-05-exos.js
+++ Semaine-02/jour-05/jour-05-exos.js
@@ -22,3 +22,13 @@ let currentUsername = "John";
 const BIRTHDAY = '18.04.1982'; // This is correct --> DoB will never change
 
 const AGE = someCode(BIRTHDAY); // This is wrong --> Age will change on page reload at Bday --> should be const age
+
+// Exercice js.info 2.5
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // Output: "hello 1"
+
+alert( `hello ${"name"}` ); // Output: "hello name
+
+alert( `hello ${name}` ); // Output: "hello Ilya"
