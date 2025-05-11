b2d04a58cba32aaa37f21aafba0bace3b85a5901
Hazel Cunuder <122833349+HazelCunuder@users.noreply.github.com>
2025-05-11 18:53:21 +0200

    feat: add josephus problem folder

---
+++ katas-js/josephus/josephus.js
@@ -0,0 +1,17 @@
+"use strict"
+
+/*
+Josephus problem is a math puzzle with a grim description:  `n`  prisoners are standing on a circle, sequentially numbered from  `1`  to  `n−1`.
+An executioner walks along the circle, starting from prisoner  0 , removing every  k -th prisoner and killing him.
+As the process goes on, the circle becomes smaller and smaller, until only one prisoner remains, who is then freed.
+For example, if there are `n=5` prisoners and `k=2`, the order the prisoners are killed in (let's call it the "killing sequence") will be 2, 4, 1, and 5, and the survivor will be #3.
+Given any `n,k>0`, find out which prisoner will be the final survivor.
+*/
+
+function josephus(n, k){
+    let prisoners = [];
+
+    for (let i = 0; i < n; i++) {
+        
+    }
+}
